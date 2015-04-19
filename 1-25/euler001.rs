2247ef57a9df3a5c19b5fb02a0f3a64edd932e3d
Vincent Wong <duperduper@ucla.edu>
2015-04-18 20:57:26 -0700

    Changed Rust 1 to loop
    Apparently Rust *doesn't* do tail-call optimization....

--- 1-25/euler001.rs
+++ 1-25/euler001.rs
@@ -1,10 +1,9 @@
-fn sum(n: i32, accum: i32) -> i32 {
-    if n <= 0 { accum }
-    else if n % 3 == 0 { sum(n-1, accum+n) }
-    else if n % 5 == 0 { sum(n-1, accum+n) }
-    else { sum(n-1, accum) }
-}
-
 fn main() {
-    println!("{}", sum(999, 0));
+    let mut sum = 0;
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+    println!("{}", sum);
 }
